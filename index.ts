e52bc76d3148475b734a8896a2369b52d2a06b94
Gabriel Pivoto <80757612+gabriel-pivoto@users.noreply.github.com>
2025-08-18 20:33:12 -0300

    Revert "Add log da cor branca"

--- index.ts
+++ index.ts
@@ -4,4 +4,3 @@ console.log(chalk.blue('Olá, mundo!'));
 console.log(chalk.red.bold('Este é um sistema simples com TypeScript.'));
 console.log(chalk.green('A dependência externa está funcionando!'));
 console.log(chalk.black('Esta é a cor preta!'));
-console.log(chalk.white('Esta é a cor branca!'));
