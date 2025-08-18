a7aab96e348c71def7747e563ca2c58b62e55d6a
Gabriel Pivoto <gabrielspivoto@gmail.com>
2025-08-18 20:34:41 -0300

    feat: add yellow log

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import chalk from 'chalk';
 
 console.log(chalk.blue('Olá, mundo!'));
-console.log(chalk.red.bold('Este é um sistema simples com TypeScript.'));
+console.log(chalk.yellow.bold('Este é um sistema simples com TypeScript.'));
 console.log(chalk.green('A dependência externa está funcionando!'));
 console.log(chalk.black('Esta é a cor preta!'));
