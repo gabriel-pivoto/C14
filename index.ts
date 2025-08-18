c4ce3ed68e96a50f72dbf507edc258adcd31d67d
FranciscoGSilverio <franciscogsilverio@gmail.com>
2025-08-18 20:29:47 -0300

    add log info

--- index.ts
+++ index.ts
@@ -3,3 +3,4 @@ import chalk from 'chalk';
 console.log(chalk.blue('Olá, mundo!'));
 console.log(chalk.red.bold('Este é um sistema simples com TypeScript.'));
 console.log(chalk.green('A dependência externa está funcionando!'));
+console.log(chalk.black('Esta é a cor preta!'));
